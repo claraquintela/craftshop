54fe74ffd8935b2076553e7fb181f7361ca8a733
Allison Hamilton <allisoneylh@gmail.com>
2019-11-17 12:57:26 -0500

    updated App string

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
-    render = () => {
-        return "Hello world!"
-    }
+  render = () => {
+    return "We are the crafty group!";
+  };
 }
 
-
-export default App
+export default App;
