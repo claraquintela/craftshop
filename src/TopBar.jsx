632f90962eaba6e63b28d8825a9d7832410ae783
Clara Quintela <claraquintela@gmail.com>
2019-11-20 13:23:01 -0500

    updating search and top bar (components and css)

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -5,48 +5,21 @@ import { Link } from "react-router-dom";
 import Search from "./Search.jsx";
 
 class UnconnectedTopBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
-  handleSubmit = async evt => {
-    evt.preventDefault();
-    console.log("login form submitted");
-    let data = new FormData();
-    data.append("username", this.state.username);
-    data.append("password", this.state.password);
-    let response = await fetch("/login", {
-      method: "POST",
-      body: data,
-      credentials: "include"
-    });
-    let responseBody = await response.text();
-    console.log("responseBody from login", responseBody);
-    let body = JSON.parse(responseBody);
-    console.log("parsed body", body);
-    if (!body.success) {
-      alert("login failed");
-      return;
-    }
-    this.props.dispatch({
-      type: "login-success"
-    });
-  };
   render = () => {
     return (
       <div>
+        <Link className="link" to="/">
+          <img
+            src="http://claraquintela.com/wp-content/uploads/2019/11/circle-cropped-1.png"
+            align="left"
+            height="150px"
+            className="topbar-image"
+          />
+        </Link>
+
         <div className="navigationBar">
-          <Link className="link" to="/">
-            <img
-              src="http://claraquintela.com/wp-content/uploads/2019/11/crafty-people-Logo.png"
-              align="left"
-              height="200px"
-            />
-          </Link>
           <Search />
+
           <section class="topbar-left">
             <Link className="link" to="/login">
               <img
