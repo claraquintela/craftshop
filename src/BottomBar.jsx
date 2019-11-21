4cad4488b9a070c52b0adfb9454af13c22cf5047
Clara Quintela <claraquintela@gmail.com>
2019-11-21 12:44:45 -0500

    updated to bar

---
+++ src/BottomBar.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./topbar.css";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Search from "./Search.jsx";
+
+class UnconnectedBottomBar extends Component {
+  render = () => {
+    return (
+      <div className="container-topbar">
+        <div>
+          <Link className="link" to="/">
+            <img
+              src="http://claraquintela.com/wp-content/uploads/2019/11/circle-cropped-1.png"
+              align="left"
+              height="200px"
+              className="topbar-image"
+            />
+          </Link>
+        </div>
+
+        <div className="navigationBar">
+          <Search />
+
+          <section class="topbar-left">
+            <Link className="link" to="/login">
+              <img
+                src="http://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png"
+                height="50px"
+                nameclass="icon"
+                aria-label="login"
+              />
+            </Link>
+            <Link className="link" to="/signup">
+              <img
+                src="http://claraquintela.com/wp-content/uploads/2019/11/signup.png"
+                height="50px"
+                nameclass="icon"
+                aria-label="signup"
+              />
+            </Link>
+            <Link className="link" to="/cart">
+              <img
+                src="http://claraquintela.com/wp-content/uploads/2019/11/shopping-cart.png"
+                height="50px"
+                nameclass="icon"
+                aria-label="shopping_cart"
+              />
+            </Link>
+          </section>
+        </div>
+      </div>
+    );
+  };
+}
+
+let BottomBar = connect()(UnconnectedBottomBar);
+export default BottomBar;
