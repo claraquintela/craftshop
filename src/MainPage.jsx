bce4f852ba7f588bbd80771d8d025c34b95b5ba5
Clara Quintela <claraquintela@gmail.com>
2019-11-22 13:59:31 -0500

    update mainpage

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -5,34 +5,13 @@ import "./mainpage.css";
 
 class UnconnectedMainPage extends Component {
   render = () => {
-    if (this.props.searchResults.length === 0) {
-      return (
-        <div className="container-mainpage">
-          <div>
-            <img
-              src="http://claraquintela.com/wp-content/uploads/2019/11/White-and-Green-Leaves-Illustration-Landscaping-Business-Card-2.png "
-              className="product_title"
-            />
-          </div>
-          <div className="body-mainpage">
-            {this.props.products.map(item => {
-              return (
-                <div className="item-container">
-                  <Link to={"/itemDescription/" + item._id}>
-                    <img src={item.image} className="mainpage-photo" />
-                    <br />
-                    {item.title}
-                    <div></div>
-                  </Link>
-                  $ {item.price}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      );
-    }
-    console.log("this.props.searchResults", this.props.searchResults);
+    let productsToDisplay =
+      this.props.searchResults.length > 0
+        ? this.props.searchResults
+        : this.props.products;
+    let userHasSearched =
+      this.props.searchQuery || this.props.minPrice || this.props.maxPrice;
+
     return (
       <div className="container-mainpage">
         <div>
@@ -41,20 +20,21 @@ class UnconnectedMainPage extends Component {
             className="product_title"
           />
         </div>
-        <div className="body-mainpage">
-          {this.props.searchResults.length ? "" : "No products found"}
-          {this.props.searchResults.map(item => {
-            return (
-              <div className="item-container">
-                <Link to={"/itemDescription/" + item._id}>
-                  <img src={item.image} className="mainpage-photo" />
-                  <br />
-                  {item.title}
-                </Link>
-                {item.description}
-              </div>
-            );
-          })}
+        <div className="mainpage-item-grid">
+          {userHasSearched && this.props.searchResults.length === 0
+            ? "No products found"
+            : productsToDisplay.map(item => {
+                return (
+                  <div className="item-container">
+                    <Link to={"/itemDescription/" + item._id}>
+                      <img src={item.image} className="mainpage-photo" />
+                      <br />
+                      {item.title}
+                    </Link>
+                    <div>{item.price}</div>
+                  </div>
+                );
+              })}
         </div>
       </div>
     );
